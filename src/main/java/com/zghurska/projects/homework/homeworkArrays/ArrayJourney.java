5704a2a76efe4302ec2e4d35208201a7edb8300e
tatiana <Sgrsk1111>
2022-02-22 23:13:48 +0200

    test

---
+++ src/main/java/com/zghurska/projects/homework/homeworkArrays/ArrayJourney.java
@@ -0,0 +1,14 @@
+package com.zghurska.projects.homework.homeworkArrays;
+
+public class ArrayJourney {
+
+    public static void main(String args[]) {
+        String [] direction = {"Rome", "Tokyo", "Saint-Petersburg", "Moskow", "Istanbul", "New-Yourk", "Madrid", "Minsk", "London", "Paris"};
+        System.out.println("Direction = " + direction.length);
+
+
+        int [] price ={150, 200, 120, 100, 115, 220, 180, 90, 160, 130};
+        System.out.println("Price = " + price.length);
+    }
+}
+
