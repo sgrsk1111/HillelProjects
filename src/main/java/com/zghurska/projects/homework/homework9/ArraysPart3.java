79cd9d3e4719700f85fec9e61697560466e86f48
tatiana <Sgrsk1111>
2022-03-10 21:47:50 +0200

    feat: add cycle

--- src/main/java/com/zghurska/projects/homework/homework9/ArraysPart3.java
+++ src/main/java/com/zghurska/projects/homework/homework9/ArraysPart3.java
@@ -2,7 +2,8 @@ package com.zghurska.projects.homework.homework9;
 
 public class ArraysPart3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
+
         char[][]arr = new char[10][10];
 
         fillArray(arr);
@@ -12,27 +13,52 @@ public class ArraysPart3 {
         printArray(arr);
         System.out.println();
 
-        fillLeftTriangle(arr);
-        printArray(arr);
-        System.out.println();
-        fillUpTriangle(arr);
-        printArray(arr);
-        System.out.println();
-        fillRightTriangle(arr);
-        printArray(arr);
-        System.out.println();
-        fillDownTriangle(arr);
-        printArray(arr);
+        for (int i =0; i>=0; i++) {
+            fillArray(arr);
+            fillDiagonals(arr);
+            fillLeftTriangle(arr);
+            printArray(arr);
+            System.out.println();
 
+            Thread.sleep(500);
 
+            fillArray(arr);
+            fillDiagonals(arr);
+            fillUpTriangle(arr);
+            printArray(arr);
+            System.out.println();
 
+            Thread.sleep(500);
+
+            fillArray(arr);
+            fillDiagonals(arr);
+            fillRightTriangle(arr);
+            printArray(arr);
+            System.out.println();
+
+            Thread.sleep(500);
+
+            fillArray(arr);
+            fillDiagonals(arr);
+            fillDownTriangle(arr);
+            printArray(arr);
+            System.out.println();
+
+            Thread.sleep(500);
+
+            fillArray(arr);
+            fillDiagonals(arr);
+            printArray(arr);
+
+            Thread.sleep(500);
+        }
     }
     private static void fillRightTriangle(char[][]arr) {
         int size= arr.length-1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (i<j & i+j>size) {
-                    arr[i][j] = '1';
+                    arr[i][j] = '+';
                 }
             }
         }
@@ -42,7 +68,7 @@ public class ArraysPart3 {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if (i<j & i+j<size) {
-                    array[i][j] = '2';
+                    array[i][j] = '+';
                 }
             }
         }
@@ -52,28 +78,28 @@ public class ArraysPart3 {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if (i>j & i+j>size) {
-                    array[i][j] = '3';
+                    array[i][j] = '+';
                 }
             }
         }
     }
     private static void fillLeftTriangle(char[][]arrays) {
-        int size= arrays.length-1;
+        int size = arrays.length - 1;
         for (int i = 0; i < arrays.length; i++) {
             for (int j = 0; j < arrays[i].length; j++) {
-                if (i>j & i+j<size) {
-                    arrays[i][j] = '4';
+                if (i > j & i + j < size) {
+                    arrays[i][j] = '+';
                 }
             }
         }
     }
     private static void fillDiagonals(char[][]array){
-        int size = array.length-1;
-        for (int i = 0; i < array.length; i++) {
+            int size = array.length - 1;
+            for (int i = 0; i < array.length; i++) {
                 array[i][i] = '+';
                 array[i][size - i] = '+';
+            }
         }
-    }
     private static void fillArray(char[][]arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
