8c97692e1f864791e23e914c89e1a9ed40cc5df5
tatiana <Sgrsk1111>
2022-03-22 19:47:50 +0200

    feat: add last task solutions

--- src/main/java/com/zghurska/projects/homework/homework10/StringMethodTwo.java
+++ src/main/java/com/zghurska/projects/homework/homework10/StringMethodTwo.java
@@ -2,6 +2,8 @@ package com.zghurska.projects.homework.homework10;
 
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringMethodTwo {
 
@@ -11,8 +13,13 @@ public class StringMethodTwo {
         String text = scanner.next();
 
         //Task 3
-        boolean result = getInfoPalindrome(text);
-        System.out.println(result);
+        boolean result1 = getInfoPalindrome(text);
+        System.out.println(result1);
+
+        //Task 4
+        boolean result2 = getInfoConsistString(text);
+        System.out.println(result2);
+
     }
 
     public static boolean getInfoPalindrome(String text) {
@@ -22,5 +29,15 @@ public class StringMethodTwo {
         newStr.reverse();
         String reversStr = newStr.toString();
         return text.equalsIgnoreCase(reversStr);
+    }
+
+    public static boolean getInfoConsistString(String text) {
+        Pattern p = Pattern.compile("[0-9]");
+        Matcher m = p.matcher(text);
+        if (text.length() == 0)
+            return false;
+        if (m.find())
+            return true;
+        return false;
     }
 }
