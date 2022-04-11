06c773da3f8637e9a8002c8ac7d89a46f625359c
tatiana <Sgrsk1111>
2022-04-11 12:05:15 +0300

    add Class Person

--- src/main/java/com/zghurska/projects/homework/professions/Person.java
+++ src/main/java/com/zghurska/projects/homework/professions/Person.java
@@ -1,15 +1,40 @@
 package com.zghurska.projects.homework.professions;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public abstract class Person {
 
     protected final String yearOfBirth;
     protected final String fullNameDrivers;
-//    protected final int age;
+    protected final int age;
 
     public Person(String yearOfBirth, String fullNameDrivers) {
         this.yearOfBirth = yearOfBirth;
         this.fullNameDrivers = fullNameDrivers;
+        age = calculateAge();
+
     }
 
+    private int calculateAge() {
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("uk"));
+        LocalDate birthDate = LocalDate.parse(yearOfBirth, formatter);
+        return Period.between(birthDate, LocalDate.from(currentDate)).getYears();
+    }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "yearOfBirth='" + yearOfBirth + '\'' +
+                ", fullNameDrivers='" + fullNameDrivers + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
+
+
+
