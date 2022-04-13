26e6809aa4cf8babba7987a3496f7ad4cae59d86
sgrsk1111 <95987668+sgrsk1111@users.noreply.github.com>
2022-04-13 14:25:33 +0300

    refactor: add correct code

--- src/main/java/com/zghurska/projects/homework/homework12/Main.java
+++ src/main/java/com/zghurska/projects/homework/homework12/Main.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Main {
 
-
     public static void main(String[] args) {
         List<Person> people = PersonFactory.generateUniquePerson();
         for (Person person : people) {
